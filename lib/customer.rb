bc28958d02a82e9f7c24375f7ece6bf373b54c96
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-26 16:35:03 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,20 @@
 class Customer
-  
+
+  attr_accessor :name, :age
+
+  @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+      @@all
+  end
+
+  def new_meal(waiter, total, tip = 0)
+    Meal.new(waiter, self, total, tip)
+  end
 end
