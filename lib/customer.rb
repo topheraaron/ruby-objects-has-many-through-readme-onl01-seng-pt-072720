2a4b92f9aa78ec00cd9114e77f6895a5895e8a0d
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-26 17:09:50 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,5 +1,4 @@
 class Customer
-
   attr_accessor :name, :age
 
   @@all = []
@@ -11,22 +10,7 @@ class Customer
   end
 
   def self.all
-      @@all
+    @@all
   end
 
-  def new_meal(waiter, total, tip = 0)
-    Meal.new(waiter, self, total, tip)
-  end
-
-  def meals
-    Meal.all.select do |meal|
-      meal.customer == self
-    end
-  end
-
-  def waiters
-    meals.map do |meal|
-      meal.waiter 
-    end
-  end
 end
